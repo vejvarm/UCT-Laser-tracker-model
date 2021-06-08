3c2d1ed4a42860973924eb622e2bcacdabc00805
Martin Vejvar <vejvarm@gmail.com>
2021-06-08 10:16:52 +0200

    feat: implement pixel wall position calculation

--- construct.py
+++ construct.py
@@ -5,8 +5,41 @@ class Environment:
     """
 
     """
-    camera_to_wall_distance = 3  # m
-    camera_resolution = (800, 600)  # (width, height) px
+    camera_resolution = (4056, 3040)  # (width, height) px
+    camera_to_wall_distance = 4.  # m
+    _chip_size = (7.564, 5.476)  # (width, height) mm
+    _crop_factor = 5.54
+    _lens_focal_length = (2.8, 12)  # mm
+
+    def __init__(self):
+        pass
+
+    def get_fov(self, c2w_distance=camera_to_wall_distance, focal_length=_lens_focal_length[0]):
+        """ calculate camera fov based on camera to wall distance and lens adjusted focal length
+
+        :param c2w_distance: (float) distance from camera to projection wall in [m]
+        :param focal_length: (float) lens focal length in [mm] (not adjusted to crop factor)
+        :return aov, fov: Tuple[float], Tuple[float] Width x Height angle of view [deg] and field of view of the camera [m]
+        """
+
+        aov = [2*np.arctan(s/(2*focal_length)) for s in self._chip_size]  # angle of view (rad)
+        fov = [2*c2w_distance*np.tan(a/2) for a in aov]  # field of view
+
+        return tuple(np.rad2deg(aov)), tuple(fov)
+
+    def get_ppm(self, fov: float, axis=0):
+        """
+
+        :param fov: (float) field of view in meters
+        :param axis: (0) axis for which fov is calculated | Horizontal (0) | Vertical (1) |
+        :return ppm: (float) pixels per meter
+        """
+
+        resolution = self.camera_resolution[int(axis)]
+
+        ppm = resolution/fov
+
+        return ppm
 
 
 class Servo:
@@ -20,13 +53,14 @@ class Servo:
 
     def __init__(self):
         self._angle = self.__default_angle
+        self.e = Environment()
 
     # noinspection PyMethodParameters
     def __enforce_bounds(foo):
         # this is obviously a decorator (go home PyCharm, you drunk)
-        def wrap(self, angle):
+        def wrap(self, angle, *args, **kwargs):
             angle = max(min(angle, self.__angle_bounds[1]), self.__angle_bounds[0])
-            out = foo(self, angle)
+            out = foo(self, angle, *args, **kwargs)
             return out
 
         return wrap
@@ -51,32 +85,52 @@ class Servo:
             self._angle = min(self._angle + self.__speed, angle)
 
     @__enforce_bounds
-    def get_dot_wall_position(self, angle):
+    def get_dot_wall_position(self, angle, fov, axis=0):
         """ calculate position of laser dot on wall based on goniometric functions
         and angles of the servo
 
         :param angle: (int) angle of servo which is perpendicular to wall [degrees]
-        :return position: (float) position of dot [meters]
+        :param fov: (float) field of view of the camera for the given axis [meters]
+        :param axis: (int) either horizontal (0) or vertical (1) axis
+        :return position: Tuple[int] position of dot [pixels]
         """
-        wall_dist = Environment.camera_to_wall_distance
+        wall_dist = self.e.camera_to_wall_distance
+        pixel_resolution = self.e.camera_resolution
 
-        return np.tan(np.deg2rad(self.__default_angle - angle))*wall_dist
-        # TODO: convert to pixel equivalent with given camera resolution
+        # aov, fov = self.e.get_fov()
+
+        meter_distance = np.tan(np.deg2rad(self.__default_angle - angle))*wall_dist
+
+        ppm = self.e.get_ppm(fov, axis)  # pixels per meter
+
+        pixel_distance = pixel_resolution[axis]/2 + ppm*meter_distance
+
+        return int(pixel_distance)
+        # TODO: TEST if correct! convert to pixel equivalent with given camera resolution
+        # TODO: Ensure boundaries
+        # TODO: Make simple GUI for showing 2D position
 
 
 class Laser:
     """
 
     """
-    def __init__(self, servos=(Servo(), Servo())):
+    def __init__(self, env=Environment(), servos=(Servo(), Servo())):
+        self._env = env
+
         self._servo_x = servos[0]
         self._servo_y = servos[1]
 
         self.angle_x = servos[0].get_angle()
         self.angle_y = servos[1].get_angle()
 
-        self.wall_pos_x = servos[0].get_dot_wall_position(self.angle_x)
-        self.wall_pos_y = servos[1].get_dot_wall_position(self.angle_y)
+        self.aov, self.fov = self._env.get_fov()  # (x, y) [deg], (x, y) [m]
+
+        self.wall_pos_x = servos[0].get_dot_wall_position(self.angle_x, self.fov[0], axis=0)
+        self.wall_pos_y = servos[1].get_dot_wall_position(self.angle_y, self.fov[1], axis=1)
+
+    def set_fov(self, fov: tuple):
+        self.fov = fov
 
     def move_x_y_tick(self, angle_x: int = None, angle_y: int = None):
         """ move servos to specific angle, with consideration of servo speed and update the current wall positions
@@ -96,8 +150,14 @@ class Laser:
         self.angle_y = self._servo_y.get_angle()
 
         # update wall positions
-        self.wall_pos_x = self._servo_x.get_dot_wall_position(self.angle_x)
-        self.wall_pos_y = self._servo_y.get_dot_wall_position(self.angle_y)
+        self.wall_pos_x = self._servo_x.get_dot_wall_position(self.angle_x, self.fov[0], axis=0)
+        self.wall_pos_y = self._servo_y.get_dot_wall_position(self.angle_y, self.fov[0], axis=1)
+
+        if angle_x == self.angle_x and angle_y == self.angle_y:
+            return True
+        else:
+            return False
+
 
 class Construct:
     """
--- tests.py
+++ tests.py
@@ -1,5 +1,17 @@
 import unittest
-from construct import Servo, Laser, Construct
+from construct import Environment, Servo, Laser, Construct
+
+class TestEnvironment(unittest.TestCase):
+    env = Environment()
+    fov = env.get_fov()
+
+    def test_get_fov(self):
+        fov = self.env.get_fov()
+        self.assertEqual(fov, self.fov)
+        print(fov)
+
+    def test_get_ppm(self):
+        pass
 
 
 class TestServo(unittest.TestCase):
@@ -34,4 +46,25 @@ class TestServo(unittest.TestCase):
         self.assertEqual(max(min(ang, self.bounds[1]), self.bounds[0]), self.servo.get_angle())
 
     def test_wall_position(self):
-        pass
+        angle = 30
+        fov = 5
+
+        pixel_distance = self.servo.get_dot_wall_position(angle, fov)
+
+        print(pixel_distance)
+
+
+class TestLaser(unittest.TestCase):
+
+    laser = Laser()
+
+    def test_move_x_y_tick(self):
+        angles_x = list(range(10, 171, 20))
+        angles_y = list(range(170, 9, -20))
+
+        print()
+        for ax, ay in zip(angles_x, angles_y):
+            done = False
+            while not done:
+                done = self.laser.move_x_y_tick(ax, ay)
+            print(f"angles: ({ax}, {ay}) | pos_x: {self.laser.wall_pos_x} | pos_y: {self.laser.wall_pos_y}")
